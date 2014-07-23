e33f82c1c184e806e847c86205ef828d7d97ae24
Robert Carlsen <rwcarlsen@gmail.com>
2014-07-23 16:25:29 -0500

    moved mesh pkg up one level. Started filling out pattern search interfaces

--- pattern/pattern.go
+++ pattern/pattern.go
@@ -1,1 +1,34 @@
 package pattern
+
+import (
+	"github.com/rwcarlsen/optim"
+	"github.com/rwcarlsen/optim/mesh"
+)
+
+type Searcher interface {
+	Search() (improved bool, bestpos []float64)
+}
+
+type Point struct {
+	Pos []float64
+	Val float64
+}
+
+type Iterator interface {
+	Iterate(p Point, ob optim.Objectiver, ev optim.Evaler, poll Poller) (Point, error)
+}
+
+type SimpleIter struct{}
+
+func (it *SimpleIter) Iterate(p Point, ob optim.Objectiver, ev optim.Evaler, poll Poller) (Point, error) {
+	panic("not implemented")
+}
+
+type Poller interface {
+	Poll(obj optim.Objectiver, ev optim.Evaler, from Point) (success bool, bestval, bestpos []float64)
+	mesh.Mesh
+}
+
+type Grid interface {
+	PollPoints(curr Point) [][]float64
+}
