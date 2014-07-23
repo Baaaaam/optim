3260e1e14f9b6b75ba7f569d522d09fbd1e6a147
Robert Carlsen <rwcarlsen@gmail.com>
2014-07-23 12:40:59 -0500

    renamed types. added bounded mesh wrapper

--- pattern/mesh/mesh.go
+++ pattern/mesh/mesh.go
@@ -15,16 +15,16 @@ type Mesh interface {
 	Nearest(p []float64) []float64
 }
 
-type SimpleMesh struct {
+type Infinite struct {
 	Origin   []float64
 	Basis    *mat64.Dense
 	Step     float64
 	inverter *mat64.Dense
 }
 
-func (sm *SimpleMesh) Nearest(p []float64) []float64 {
+func (sm *Infinite) Nearest(p []float64) []float64 {
 	if sm.Step == 0 {
-		panic("SimpleMesh has step size 0")
+		panic("Infinite has step size 0")
 	} else if sm.Origin != nil && len(sm.Origin) != len(p) {
 		panic("point passed to Nearest is has wrong length")
 	}
@@ -65,4 +65,33 @@ func (sm *SimpleMesh) Nearest(p []float64) []float64 {
 	// transform back to standard space
 	rotv.Mul(sm.Basis, nearest)
 	return rotv.Col(nil, 0)
+}
+
+type Bounded struct {
+	Lower []float64
+	Upper []float64
+	core  Mesh
+}
+
+func NewBounded(m Mesh, lower, upper []float64) *Bounded {
+	if len(lower) != len(upper) {
+		panic("mesh lower and upper bound vectors have difference lengths")
+	} else { // force panic if bounds lengths don't math mesh m's # dims
+		m.Nearest(lower)
+	}
+	return &Bounded{
+		Lower: lower,
+		Upper: upper,
+		core:  m,
+	}
+}
+
+func (m *Bounded) Nearest(p []float64) []float64 {
+	pdup := make([]float64, len(p))
+	copy(pdup, p)
+	for i := range pdup {
+		pdup[i] = math.Max(m.Lower[i], pdup[i])
+		pdup[i] = math.Min(m.Upper[i], pdup[i])
+	}
+	return m.core.Nearest(pdup)
 }
