a278102fa838119041fd98d559a72ac7f5f107ff
Robert Carlsen <rwcarlsen@gmail.com>
2014-07-26 15:26:51 -0500

    added cache evaler test

--- optim_test.go
+++ optim_test.go
@@ -8,6 +8,11 @@ import (
 
 const errcount = 3
 
+var tpoint = Point{
+	Pos: []float64{1, 2, 3},
+	Val: 0,
+}
+
 type ErrObj struct {
 	count int
 }
@@ -24,7 +29,7 @@ func TestSerialEvalerErr(t *testing.T) {
 	obj := &ErrObj{}
 	ev := SerialEvaler{}
 
-	results, n, err := ev.Eval(obj, Point{}, Point{}, Point{}, Point{}, Point{})
+	results, n, err := ev.Eval(obj, tpoint, tpoint, tpoint, tpoint, tpoint)
 	if len(results) != errcount {
 		t.Errorf("returned wrong number of results: expected %v, got %v", errcount, len(results))
 	}
@@ -34,5 +39,21 @@ func TestSerialEvalerErr(t *testing.T) {
 	if err == nil {
 		t.Errorf("did not propogate error through return")
 	}
+}
 
+func TestCacheEvalerErr(t *testing.T) {
+	obj := &ErrObj{}
+	ev := NewCacheEvaler(SerialEvaler{})
+	r1, n1, _ := ev.Eval(obj, tpoint)
+	r2, n2, err := ev.Eval(obj, tpoint, tpoint, tpoint, tpoint)
+
+	if v := len(r1) + len(r2); v != errcount {
+		t.Errorf("returned wrong number of results: expected %v, got %v", errcount, v)
+	}
+	if n1+n2 != 1 {
+		t.Errorf("returned wrong evaluation count: expected 1, got %v", n1+n2)
+	}
+	if err != nil {
+		t.Errorf("failed to prevent extra evaluations")
+	}
 }
