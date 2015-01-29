3ed9136dfa8b1c8e960e7a9fb6abda48089cd08d
Robert Carlsen <rwcarlsen@gmail.com>
2015-01-29 13:39:52 -0600

    Added tests for ParallelEvaler. Fixed bugs.
    ParallelEvaler was rather broken. Tests caught the bugs. Improved evaler
    documentation and also improved serialevaler tests.

--- optim.go
+++ optim.go
@@ -49,6 +49,8 @@ func (p Point) At(i int) float64 { return p.pos[i] }
 
 func (p Point) Len() int { return len(p.pos) }
 
+func (p Point) Clone() Point { return NewPoint(p.pos, p.Val) }
+
 func (p Point) Matrix() *mat64.Dense {
 	return mat64.NewDense(p.Len(), 1, p.Pos())
 }
@@ -84,8 +86,10 @@ type Iterator interface {
 
 type Evaler interface {
 	// Eval evaluates each point using obj and returns the values and number
-	// of function evaluations n.  Unevaluated points should not be returned
-	// in the results slice.
+	// of times obj was called n.  If unevaluated points are returned
+	// in the results slice, they should have objective value set to
+	// +Infinity. The order of points in results must be the same as the order
+	// of the passed in points.  len(results) may be less than len(points).
 	Eval(obj Objectiver, points ...Point) (results []Point, n int, err error)
 }
 
@@ -114,6 +118,7 @@ func NewCacheEvaler(ev Evaler) *CacheEvaler {
 }
 
 func (ev *CacheEvaler) Eval(obj Objectiver, points ...Point) (results []Point, n int, err error) {
+	results = points
 	fromnew := make([]int, 0, len(points))
 	newp := make([]Point, 0, len(points))
 	for i, p := range points {
@@ -124,6 +129,7 @@ func (ev *CacheEvaler) Eval(obj Objectiver, points ...Point) (results []Point, n
 		} else {
 			fromnew = append(fromnew, i)
 			newp = append(newp, p)
+			results[i].Val = math.Inf(1) // to be safe
 		}
 	}
 
@@ -133,15 +139,16 @@ func (ev *CacheEvaler) Eval(obj Objectiver, points ...Point) (results []Point, n
 	}
 
 	for i, p := range newresults {
-		points[fromnew[i]].Val = p.Val
+		results[fromnew[i]].Val = p.Val
 	}
 
 	// shrink if error resulted in fewer new results being returned
 	if len(fromnew) > 0 {
-		points = points[:fromnew[len(newresults)-1]+1]
+		i := fromnew[len(newresults)-1]
+		results = results[:i+1]
 	}
 
-	return points, n, err
+	return results, n, err
 }
 
 type SerialEvaler struct {
@@ -149,51 +156,79 @@ type SerialEvaler struct {
 }
 
 func (ev SerialEvaler) Eval(obj Objectiver, points ...Point) (results []Point, n int, err error) {
-	results = make([]Point, 0, len(points))
-	for _, p := range uniqof(points) {
+	results = make([]Point, len(points))
+
+	indexes := uniqof(points)
+	for i, p := range points {
+		if indexes[i] != i {
+			// skip duplicates
+			continue
+		}
+
 		p.Val, err = obj.Objective(p.Pos())
-		results = append(results, p)
+		n++
+		results[i] = p
 		if err != nil && !ev.ContinueOnErr {
-			return results, len(results), err
+			fillfromuniq(indexes, results)
+			return results[:i+1], n, err
 		}
 	}
-	return results, len(results), nil
+
+	fillfromuniq(indexes, results)
+	return results, n, nil
 }
 
 type errpoint struct {
 	Point
-	Err error
+	Index int
+	Err   error
+}
+
+// uniqof returns a set of indexes that map a point index into another index
+// in the same point slice that can be used go get objective values of
+// duplicate positions.  If a point has a unique position, indexes[i] = i.
+func uniqof(points []Point) (indexes []int) {
+	indexes = make([]int, len(points))
+	alreadyhave := map[[sha1.Size]byte]int{}
+	for i, p := range points {
+		h := hashPoint(p)
+		if v, ok := alreadyhave[h]; !ok {
+			alreadyhave[h] = i
+			indexes[i] = i
+		} else {
+			indexes[i] = v
+		}
+	}
+	return indexes
+}
+
+func fillfromuniq(indexes []int, points []Point) {
+	for i := range points {
+		if v := indexes[i]; v != i {
+			points[i] = points[v].Clone()
+		}
+	}
 }
 
 type ParallelEvaler struct{}
 
-func uniqof(points []Point) []Point {
-	uniq := make([]Point, 0, len(points))
-	alreadyhave := map[[sha1.Size]byte]bool{}
-	for _, p := range points {
-		h := hashPoint(p)
-		if !alreadyhave[h] {
-			alreadyhave[h] = true
-			uniq = append(uniq, p)
-		}
-	}
-	return uniq
-}
-
 func (ev ParallelEvaler) Eval(obj Objectiver, points ...Point) (results []Point, n int, err error) {
-	results = make([]Point, 0, len(points))
 	ch := make(chan errpoint, len(points))
 	wg := sync.WaitGroup{}
-	uniq := uniqof(points)
-	wg.Add(len(uniq))
+	indexes := uniqof(points)
+	for i, p := range points {
+		if indexes[i] != i {
+			// skip duplicates
+			continue
+		}
 
-	for _, p := range uniq {
-		go func() {
-			perr := errpoint{Point: p}
+		wg.Add(1)
+		go func(i int, p Point) {
+			defer wg.Done()
+			perr := errpoint{Point: p, Index: i}
 			perr.Val, perr.Err = obj.Objective(p.Pos())
 			ch <- perr
-			wg.Done()
-		}()
+		}(i, p)
 	}
 
 	go func() {
@@ -201,13 +236,16 @@ func (ev ParallelEvaler) Eval(obj Objectiver, points ...Point) (results []Point,
 		close(ch)
 	}()
 
+	results = make([]Point, len(points))
 	for p := range ch {
+		n++
+		results[p.Index] = p.Point
 		if p.Err != nil {
 			err = p.Err
 		}
-		results = append(results, p.Point)
 	}
-	return results, len(results), err
+	fillfromuniq(indexes, results)
+	return results, n, err
 }
 
 type Func func([]float64) float64
--- optim_test.go
+++ optim_test.go
@@ -3,11 +3,13 @@ package optim
 import (
 	"errors"
 	"math"
+	"sync"
 	"testing"
 )
 
 var tpoints = []Point{
 	NewPoint([]float64{1, 2, 3}, 0),
+	NewPoint([]float64{1, 2, 3}, 0), // duplicate point on purpose
 	NewPoint([]float64{1, 2, 4}, 0),
 	NewPoint([]float64{1, 2, 5}, 0),
 	NewPoint([]float64{1, 2, 6}, 0),
@@ -17,9 +19,13 @@ var tpoints = []Point{
 type ObjTest struct {
 	count int
 	max   int
+	sync.Mutex
 }
 
 func (o *ObjTest) Objective(x []float64) (float64, error) {
+	o.Lock()
+	defer o.Unlock()
+
 	o.count++
 	if o.count >= o.max {
 		return math.Inf(1), errors.New("fake error")
@@ -33,34 +39,84 @@ func (o *ObjTest) Objective(x []float64) (float64, error) {
 
 func TestSerialEvalerErr(t *testing.T) {
 	errcount := 3
+	exprlen := errcount + 1 // we get an extra obj call due to duplicate avoidance
+	expn := exprlen - 1
 	obj := &ObjTest{max: errcount}
 	ev := SerialEvaler{}
 
 	r, n, err := ev.Eval(obj, tpoints...)
 
-	if len(r) != errcount {
-		t.Errorf("returned wrong number of r: expected %v, got %v", errcount, len(r))
+	if len(r) != exprlen {
+		// if this fires, duplicate point avoidance may be broken
+		t.Errorf("returned wrong number of results: expected %v, got %v", exprlen, len(r))
 	}
-	if n != errcount {
+	if n != expn {
+		t.Errorf("returned wrong evaluation count: expected %v, got %v", expn, n)
+	}
+	if err == nil {
+		t.Errorf("did not propagate error through return")
+	}
+
+	// exclude last entry in r because it was the error'd obj evaluation
+	for i, p := range r[:len(r)-1] {
+		expobj := 0.0
+		for _, v := range tpoints[i].Pos() {
+			expobj += v
+		}
+		if p.Val != expobj {
+			t.Errorf("point %v (%v) objective value: expected %v, got %v", i, tpoints[i].Pos(), expobj, p.Val)
+		}
+	}
+}
+
+func TestParallelEvalerErr(t *testing.T) {
+	errcount := 4
+	exprlen := len(tpoints)
+	expn := exprlen - 1
+	obj := &ObjTest{max: errcount}
+	ev := ParallelEvaler{}
+
+	r, n, err := ev.Eval(obj, tpoints...)
+
+	// parallel always evaluates all points
+	if len(r) != exprlen {
+		t.Errorf("returned wrong number of results: expected %v, got %v", exprlen, len(r))
+	}
+	if n == len(tpoints) {
+		t.Errorf("failed to avoid evaluation of duplicate points", errcount, n)
+	}
+	if n != expn {
 		t.Errorf("returned wrong evaluation count: expected %v, got %v", errcount, n)
 	}
 	if err == nil {
-		t.Errorf("did not propogate error through return")
+		t.Errorf("did not propagate error through return")
+	}
+
+	for i, p := range r[:errcount-1] {
+		expobj := 0.0
+		for _, v := range tpoints[i].Pos() {
+			expobj += v
+		}
+		if p.Val != expobj {
+			t.Errorf("point %v (%v) objective value: expected %v, got %v", i, tpoints[i].Pos(), expobj, p.Val)
+		}
 	}
 }
 
 func TestCacheEvalerErr(t *testing.T) {
 	errcount := 3
+	exprlen := errcount + 1 // we get an extra obj call due to duplicate avoidance
+	expn := exprlen - 1
 	obj := &ObjTest{max: errcount}
 	ev := NewCacheEvaler(SerialEvaler{})
 
 	r, n, err := ev.Eval(obj, tpoints...)
 
-	if len(r) != errcount {
-		t.Errorf("returned wrong number of r: expected %v, got %v", errcount, len(r))
+	if len(r) != exprlen {
+		t.Errorf("returned wrong number of r: expected %v, got %v", exprlen, len(r))
 	}
-	if n != errcount {
-		t.Errorf("returned wrong evaluation count: expected %v, got %v", errcount, n)
+	if n != expn {
+		t.Errorf("returned wrong evaluation count: expected %v, got %v", expn, n)
 	}
 	if err == nil {
 		t.Errorf("did not propogate error through return")
@@ -70,6 +126,7 @@ func TestCacheEvalerErr(t *testing.T) {
 func TestCacheEvaler(t *testing.T) {
 	obj := &ObjTest{max: 100000}
 	ev := NewCacheEvaler(SerialEvaler{})
+	expn := len(tpoints) - 1
 
 	r1, n1, err1 := ev.Eval(obj, tpoints...)
 	r2, n2, err2 := ev.Eval(obj, tpoints...)
@@ -77,8 +134,8 @@ func TestCacheEvaler(t *testing.T) {
 	if v := len(r1) + len(r2); v != 2*len(tpoints) {
 		t.Errorf("returned wrong number of results: expected %v, got %v", 2*len(tpoints), v)
 	}
-	if n1+n2 != len(tpoints) {
-		t.Errorf("returned wrong evaluation count: expected %v, got %v", len(tpoints), n1+n2)
+	if n1+n2 != expn {
+		t.Errorf("returned wrong evaluation count: expected %v, got %v", expn, n1+n2)
 	}
 	if err1 != nil || err2 != nil {
 		t.Errorf("got unexpected err (err1 and err2): %v and %v", err1, err2)
